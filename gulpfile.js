91962e0a94a9920ec416d9da36f0c59a8a39f996
Jonny Spry <jspry@energyinst.org>
2017-10-31 16:27:47 +0000

    integrate browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,35 @@
 // Once the package is found, we assign its contents to the variable gulp
 var gulp = require('gulp');
 
-// installs sass css preprocessor compiler
+// includes sass css preprocessor compiler
 var sass = require('gulp-sass');
 
+// includes browsersync
+var browserSync = require('browser-sync').create();
 
-gulp.task('sass', function(){
-  return gulp.src('app/scss/**/*.scss')
-    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+// compiles scss files into css and dumps them into the css folder
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
     .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('watch', function(){
+// inits browserSync
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+// 'watches' html, scss and js files, and uses browserSync to reload page when any files are saved
+gulp.task('watch', ['browserSync'], function (){
   gulp.watch('app/scss/**/*.scss', ['sass']); 
   // Other watchers
+  gulp.watch('app/*.html').on('change', browserSync.reload);
+  gulp.watch('app/js/**/*.js').on('change', browserSync.reload);
 })
