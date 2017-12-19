d8f263710feae6a5d647febbf806098aa8dd562f
Jonny Spry <jspry@energyinst.org>
2017-12-19 15:25:59 +0000

    remove babel from project

--- app/es6/script.js
+++
@@ -1,6 +0,0 @@
-
-var demo = document.getElementById('demo');
-
-var myFunc = (text, el) => { el.innerHTML = text};
-
-myFunc("I made it in the show ... es6 - coming from the future!" + '<br>' + " - this script is compiled from " + '<span style="font-style:italic";>app/es6 directory</span>' + '<br>' + "and plain old es5 dumped into " + '<span style="font-style:italic";>app/js/directory</span>', demo);
--- gulpfile.js
+++ gulpfile.js
@@ -8,18 +8,6 @@ var sass = require('gulp-sass');
 // includes browsersync
 var browserSync = require('browser-sync').create();
 
-// allows babel to compile es6 code into es5
-const babel = require('gulp-babel');
- 
-gulp.task('compile-js', () => {
-    console.log('babel task')
-    gulp.src('app/es6/script.js')
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(gulp.dest('app/js'))
-});
-
 // compiles scss files into css and dumps them into the css folder
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
@@ -41,9 +29,8 @@ gulp.task('browserSync', function() {
 
 // 'watches' html, scss and js files, and uses browserSync to reload page when any files are saved
 gulp.task('watch', ['browserSync'], function (){
-  // compile scss and es6
+  // compile scss
   gulp.watch('app/scss/**/*.scss', ['sass']); 
-  gulp.watch('app/es6/**/*.js', ['compile-js']); 
   // reload browser
   gulp.watch('app/*.html').on('change', browserSync.reload);
   gulp.watch('app/css/**/*.css').on('change', browserSync.reload);
